d4c95114d7bbaf78765d6689c18168c083022519
Ed Daly <edaly@generaldigital.com>
2023-12-18 15:52:28 -0500

    many changes

--- Makefile
+++ Makefile
@@ -1,31 +1,27 @@
-# Makefile for daptest
-# Collection of test programs to test the DAP APIs
-#
-# Assunes:
-# All programs begin with "test" and end with ".c"
-#
+# Makefile for fproftest
 # To build: "make" or "make all"
 # To clean: "make clean"
 
 .PHONY: all clean
 
-ALLEGRO_FLAGS=-I/usr/local/include/allegro5
-DAP_FLAGS=-I/usr/local/include/dap
-FONT_FLAGS=-I/usr/local/include/gdcfonts
+#ALLEGRO_FLAGS=-I/usr/local/include/allegro5
+#DAP_FLAGS=-I/usr/local/include/dap
+#FONT_FLAGS=-I/usr/local/include/gdcfonts
 
 CC=gcc
-CFLAGS=-Wall -O0 -ggdb -std=gnu99 -DDEBUG
+CFLAGS=-Wall -O0 -ggdb -std=gnu99 -DDEBUG -finstrument-functions -export-dynamic -Wl,--export-dynamic
 DFLAGS=-MP -MD
 
-TLIBS=-L/usr/local/lib/gdcfonts -Wl,-R/usr/local/lib/gdcfonts -lgdcfonts
-TLIBS+=-L/usr/local/lib/dap -Wl,-R/usr/local/lib/dap -ldapterm -ldapiou
-TLIBS+=-L/usr/local/lib -Wl,-R/usr/local/lib -lallegro_primitives -lallegro -lallegro_image
+#TLIBS=-L/usr/local/lib/gdcfonts -Wl,-R/usr/local/lib/gdcfonts -lgdcfonts
+#TLIBS+=-L/usr/local/lib/dap -Wl,-R/usr/local/lib/dap -ldapterm -ldapiou
+#TLIBS+=-L/usr/local/lib -Wl,-R/usr/local/lib -lallegro_primitives -lallegro -lallegro_image
 #TLIBS+=-lallegro_color -lallegro_main
-TLIBS+=-lpthread -lm
+TLIBS=-L/usr/local/lib -Wl,-R/usr/local/lib -lfprof
+TLIBS+= -lpthread -lm -ldl
 
 # Test builds
 TSRC=src
-TSRCS=$(wildcard $(TSRC)/test*.c)
+TSRCS=$(wildcard $(TSRC)/*.c)
 TBIN=bin
 TBINS=$(patsubst $(TSRC)/%.c, $(TBIN)/%, $(TSRCS))
 TDEPFILES=$(patsubst $(TSRC)/%.c, $(TBIN)/%.d, $(TSRCS))
@@ -36,7 +32,7 @@ all: $(TBINS)
 
 $(TBIN)/%: $(TSRC)/%.c
 	@echo "=== Building Test program $<"
-	$(CC) -o $@ $< $(CFLAGS) $(DFLAGS) $(FONT_FLAGS) $(DAP_FLAGS) $(ALLEGRO_FLAGS) $(TLIBS)
+	$(CC) -o $@ $< $(CFLAGS) $(DFLAGS) $(TLIBS)
 
 clean:
 	@echo "=== Clean everything"
---
+++ src/fproftest.c
@@ -0,0 +1,101 @@
+// fproftest.c
+// test of using fprof, function profile utility
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <fprof.h>
+
+
+//#if 0
+#define MAX_LEN	64
+#define MAX_FUNCTIONS	4096
+
+
+enum ETIME {START = 0, END = 1};
+
+typedef void(*cb_func_c)(char*);
+
+struct timeval start, end;
+
+struct DAP_PATTERN_LUT{
+    char *pattern;
+    cb_func_c cb;
+};
+
+void callback1(char *s);
+
+struct DAP_PATTERN_LUT relut[] = {
+    {"033A", &callback1},
+};
+
+void callback1(char *s){
+    fprintf(stdout, "callback1 function called, pattern = %s\n", s);
+    return;
+}
+
+void do_nothing1(void) {
+	printf("do nothing 1\n");
+	(*relut[0].cb)(relut[0].pattern);
+}
+
+void do_nothing2(void) {
+	printf("do nothing 2\n");
+	(*relut[0].cb)(relut[0].pattern);
+}
+
+void do_something(void) {
+	(*relut[0].cb)(relut[0].pattern);
+}
+
+// Determine elapse time
+double elapsed_time(enum ETIME sts, struct timeval *start, struct timeval *end)
+{
+	long long startusec = 0, endusec = 0;
+	double elapsed = 0;
+
+	do_nothing1();
+
+	do_nothing2();
+
+	do_something();
+
+	(*relut[0].cb)(relut[0].pattern);
+
+	if (sts == START)
+	{
+		gettimeofday(start, NULL);
+	}
+	else
+	{
+		gettimeofday(end, NULL);
+		startusec = start->tv_sec * 1000000 + start->tv_usec;
+		endusec = end->tv_sec * 1000000 + end->tv_usec;
+		elapsed = (double)(endusec - startusec) / 1000000.0;	// seconds
+		elapsed = (double)(endusec - startusec);				// usec
+	}
+	return elapsed;
+}
+
+
+int main(int argc, char *argv[])
+{
+	double elapsedt=0;
+
+	elapsedt = elapsed_time(START, &start, &end);
+	sleep(1);
+	elapsedt = elapsed_time(END, &start, &end);
+
+	fprintf(stdout, "Elapsed time is %f usec\n", elapsedt);
+
+	while(1);
+	exit(EXIT_SUCCESS);
+
+}
+
+
+
